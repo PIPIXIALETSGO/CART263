02223754d4c376c618110506c4b4f09a1ddb359c
John Wu <haozhu197@hotmail.com>
2023-01-14 16:01:20 -0500

    update

--- excercise1/js/script.js
+++ excercise1/js/script.js
@@ -22,15 +22,15 @@ var horizontal = 90;
 var counter = 0;
 var player = ["X", "O"];
 var currPlayer;
-var xScore=0
-var yScore=0
-var winner=false
+var xScore = 0;
+var oScore = 0;
+var winner = false;
+var reset=false
 var board = [
-  [" ", "", " "],    //[0][0],[0][1],[0][2]
-  ["", " ", ""],     //[1][0],[1][1],[1][2]
-  ["", " ", ""],     //[2][0],[2][1],[2][2]
+  [" ", "", " "], //[0][0],[0][1],[0][2]
+  ["", " ", ""], //[1][0],[1][1],[1][2]
+  ["", " ", ""], //[2][0],[2][1],[2][2]
 ];
-var score = "0";
 function setup() {
   createCanvas(900, 1100);
   background(0);
@@ -45,36 +45,17 @@ function setup() {
 
   line(300, 0, 300, 900);
   line(600, 0, 600, 900);
+  fill(255)
 }
 
 /**
 Description of draw()
 */
 function draw() {
-
-  textSize(200);
-
-  vertical = 225;
-  for (var ii = 0; ii < 3; ii++) {
-    horizontal = 80;
-    for (var jj = 0; jj < 3; jj++) {
-      text(board[ii][jj], horizontal, vertical);
-      horizontal += 300;
-    }
-    vertical += 300;
-  }
+  drawSymbol();
   checkResult();
-  textSize(100);
-  displayMessage()
-  textSize(50);
-  text('X',50,950)
-  text('Y',250,950)
-  text('Y',250,950)
-  textSize(80);
-  text(':',155,1060)
-  text(xScore,50,1065)
-  text(yScore,250,1065)
-  
+  displayMessage();
+  displayScore();
 }
 function mouseClicked() {
   if (counter % 2 === 0) {
@@ -118,36 +99,90 @@ function mouseClicked() {
   }
   counter++;
 }
+
+function drawSymbol() {
+    if(reset===false){
+        textSize(200);
+        vertical = 225;
+        for (var ii = 0; ii < 3; ii++) {
+          horizontal = 80;
+          for (var jj = 0; jj < 3; jj++) {
+            text(board[ii][jj], horizontal, vertical);
+            horizontal += 300;
+          }
+          vertical += 300;
+        }
+    }else{
+        drawBoard()
+    }
+  
+}
 function checkResult() {
-    for(var ii=0;ii<3;ii++){
-        if (board[ii][0] === board[ii][1] && board[ii][0] === board[ii][2]) {
-            winner=true
-          }
-          if (board[0][ii] === board[1][ii] && board[0][ii] === board[2][ii]) {
-            winner=true
-          }
+  for (var ii = 0; ii < 3; ii++) {
+    if (board[ii][0] === board[ii][1] && board[ii][0] === board[ii][2]) {
+      winner = true;
     }
-    if(board[0][0] === board[1][1]&&board[0][0] === board[2][2]){
-        winner=true
+    if (board[0][ii] === board[1][ii] && board[0][ii] === board[2][ii]) {
+      winner = true;
     }
-    if(board[2][0] === board[1][1]&&board[2][0] === board[0][2]){
-        winner=true
-    }
+  }
+  if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+    winner = true;
+  }
+  if (board[2][0] === board[1][1] && board[2][0] === board[0][2]) {
+    winner = true;
+  }
 }
-function displayMessage(){
-    if(winner){
-        text(currPlayer + "  won", 550, 1050);
-        setTimeout(resetGame,2000);
-    }
+function displayMessage() {
+  textSize(100);
+  if (winner) {
+    text(currPlayer + "  won", 550, 1050);
+    
+    setTimeout(resetGame, 1000);
+  }
 }
-function resetGame(){
-    console.log('wow')
-    winner=false
-    counter=0
-     board = [
-        [" ", "", " "],  
-        ["", " ", ""],   
-        ["", " ", ""],   
-      ]
-     
+function resetGame() {
+  console.log(board);
+  winner = false;
+  counter = 0;
+  board = [
+    [" ", "", " "], //[0][0],[0][1],[0][2]
+    ["", " ", ""], //[1][0],[1][1],[1][2]
+    ["", " ", ""], //[2][0],[2][1],[2][2]
+  ]
+  reset=true
+}
+function displayScore() {
+  textSize(70);
+  text("X", 50, 970);
+  text("O", 250, 970);
+  textSize(80);
+  text(":", 155, 1061);
+  text(xScore, 50, 1065);
+  text(oScore, 250, 1065);
+}
+function drawBoard(){
+    if(reset){
+        background(0);
+        strokeWeight(5);
+        stroke(255);
+        line(0, 300, 900, 300);
+        line(0, 600, 900, 600);
+        strokeWeight(10);
+        line(0, 900, 900, 900);
+      
+        strokeWeight(5);
+      
+        line(300, 0, 300, 900);
+        line(600, 0, 600, 900);
+    }
+    reset=false
+    
+}
+
+//////////////////miss adding score///////////////
+if(currPlayer==="X"){
+    xScore+=1
+}else{
+    yScore+=1
 }

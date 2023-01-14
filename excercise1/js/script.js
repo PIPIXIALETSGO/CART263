964b00eeade922a9cb72c10913509ca064d9dcb3
John Wu <haozhu197@hotmail.com>
2023-01-14 18:24:08 -0500

    commenting

--- excercise1/js/script.js
+++ excercise1/js/script.js
@@ -1,22 +1,6 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Initializing all the variables
 */
-
-"use strict";
-
-/**
-Description of preload
-*/
-function preload() {}
-
-/**
-Description of setup
-*/
-
 var vertical = 225;
 var horizontal = 90;
 var counter = 0;
@@ -25,37 +9,45 @@ var currPlayer;
 var xScore = 0;
 var oScore = 0;
 var winner = false;
-var reset=false
+var reset = false;
 var board = [
   [" ", "", " "], //[0][0],[0][1],[0][2]
   ["", " ", ""], //[1][0],[1][1],[1][2]
   ["", " ", ""], //[2][0],[2][1],[2][2]
 ];
+function preload() {}
+
 function setup() {
   createCanvas(900, 1100);
   background(0);
   strokeWeight(5);
   stroke(255);
+
   line(0, 300, 900, 300);
   line(0, 600, 900, 600);
   strokeWeight(10);
   line(0, 900, 900, 900);
-
+  /**
+Drawing horizontal line
+*/
   strokeWeight(5);
 
   line(300, 0, 300, 900);
   line(600, 0, 600, 900);
-  fill(255)
+  fill(255);
+  /**
+Drawing vertical line
+*/
 }
 
-/**
-Description of draw()
-*/
 function draw() {
   drawSymbol();
   checkResult();
   displayMessage();
   displayScore();
+  /**
+Calling all the functions to do their things
+*/
 }
 function mouseClicked() {
   if (counter % 2 === 0) {
@@ -63,7 +55,9 @@ function mouseClicked() {
   } else {
     currPlayer = player[1];
   }
-
+  /**
+change the turn whenever mouse is clicked
+*/
   if (mouseX > 0 && mouseX < 300) {
     if (mouseY > 0 && mouseY < 300) {
       board[0][0] = currPlayer;
@@ -97,26 +91,31 @@ function mouseClicked() {
       board[2][2] = currPlayer;
     }
   }
+  /**
+   display a message when someone wins
+*/
   counter++;
 }
 
 function drawSymbol() {
-    if(reset===false){
-        textSize(200);
-        vertical = 225;
-        for (var ii = 0; ii < 3; ii++) {
-          horizontal = 80;
-          for (var jj = 0; jj < 3; jj++) {
-            text(board[ii][jj], horizontal, vertical);
-            horizontal += 300;
-          }
-          vertical += 300;
-        }
-    }else{
-        drawBoard()
+  if (reset === false) {
+    textSize(200);
+    vertical = 225;
+    for (var ii = 0; ii < 3; ii++) {
+      horizontal = 80;
+      for (var jj = 0; jj < 3; jj++) {
+        text(board[ii][jj], horizontal, vertical);
+        horizontal += 300;
+      }
+      vertical += 300;
     }
-  
+  } else {
+    drawBoard();
+  }
 }
+/**
+   draw all the symbols from the array
+*/
 function checkResult() {
   for (var ii = 0; ii < 3; ii++) {
     if (board[ii][0] === board[ii][1] && board[ii][0] === board[ii][2]) {
@@ -133,6 +132,9 @@ function checkResult() {
     winner = true;
   }
 }
+/**
+   check all the winning condition (whenever there's a tripple on horizontal,vertical and diagonal
+*/
 function displayMessage() {
   textSize(100);
   if (winner) {
@@ -140,54 +142,66 @@ function displayMessage() {
     setTimeout(resetGame, 1000);
   }
 }
+/**
+   display a message when someone wins
+*/
 function resetGame() {
   counter = 0;
-  addScore()
-  currPlayer='X'
-  winner=false
+  addScore();
+  currPlayer = "X";
+  winner = false;
   board = [
     [" ", "", " "], //[0][0],[0][1],[0][2]
     ["", " ", ""], //[1][0],[1][1],[1][2]
     ["", " ", ""], //[2][0],[2][1],[2][2]
-  ]
-  reset=true
+  ];
+  reset = true;
 }
+/**
+   reset all the variables so it can run a new game
+*/
 function displayScore() {
   textSize(70);
   text("X", 50, 970);
-  text("O", 250, 970);
+  text("O", 235, 970);
+  textSize(55);
+  text(":", 155, 1055);
   textSize(80);
-  text(":", 155, 1061);
   text(xScore, 50, 1065);
-  text(oScore, 250, 1065);
+  text(oScore, 235, 1065);
 }
-function drawBoard(){
-    if(reset){
-        background(0);
-        strokeWeight(5);
-        stroke(255);
-        line(0, 300, 900, 300);
-        line(0, 600, 900, 600);
-        strokeWeight(10);
-        line(0, 900, 900, 900);
-      
-        strokeWeight(5);
-      
-        line(300, 0, 300, 900);
-        line(600, 0, 600, 900);
-    }
-    reset=false
-    
-}
-function addScore(){
-    if(winner){
-        if(currPlayer==="X"){
-            xScore+=1
-        }else{
-            oScore+=1
-        }
-    }
+/**
+  display the score on the buttom
+*/
+function drawBoard() {
+  if (reset) {
+    background(0);
+    strokeWeight(5);
+    stroke(255);
+    line(0, 300, 900, 300);
+    line(0, 600, 900, 600);
+    strokeWeight(10);
+    line(0, 900, 900, 900);
 
-    
+    strokeWeight(5);
+
+    line(300, 0, 300, 900);
+    line(600, 0, 600, 900);
+  }
+  reset = false;
 }
-//////////////////miss adding score///////////////
+/**
+   clear out all the symbols on the board, and make a new board
+*/
+function addScore() {
+  if (winner) {
+    if (currPlayer === "X") {
+      xScore += 1;
+    } else {
+      oScore += 1;
+    }
+  }
+}
+/**
+   add 1 score to the winner
+*/

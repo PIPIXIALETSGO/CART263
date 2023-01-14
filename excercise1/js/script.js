86107d7b4ba4057d4af03f4e19434a5e70907c1d
John Wu <haozhu197@hotmail.com>
2023-01-14 14:16:56 -0500

    Update

--- excercise1/js/script.js
+++ excercise1/js/script.js
@@ -8,27 +8,54 @@ author, and this description to match your project!
 
 "use strict";
 
-
 /**
 Description of preload
 */
-function preload() {
-
-}
-
+function preload() {}
 
 /**
 Description of setup
 */
-function setup() {
-createCanvas(500,500)
-background(97,1,27)
-}
 
+var vertical = 225;
+var horizontal = 90;
+
+var board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+function setup() {
+  createCanvas(900, 900);
+  background(0);
+  strokeWeight(5);
+  stroke(255);
+  line(0, 300, 900, 300);
+  line(0, 600, 900, 600);
+
+  line(300, 0, 300, 900);
+  line(600, 0, 600, 900);
+  textSize(200);
+}
 
 /**
 Description of draw()
 */
 function draw() {
-
+  for (var ii = 0; ii < 3; ii++) {
+    horizontal = 80;
+    for (var jj = 0; jj < 3; jj++) {
+      text(board[ii][jj], horizontal, vertical);
+      horizontal += 300;
+    }
+    vertical += 300;
+  }
+}
+function mouseClicked() {
+  if (mouseX > 0 && mouseX < 300) {
+    console.log(board);
+    board[0][0] = "X";
+    board[0][1] = "X";
+    board[0][2] = "X";
+  }
 }
